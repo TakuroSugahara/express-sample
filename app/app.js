fdf1dc088256d390e88a1e2cc9c1a707f55bb157
TakuroSugahara <k5690033@gmail.com>
2020-04-13 11:17:33 +0900

    first commit

---
+++ app/app.js
@@ -0,0 +1,22 @@
+// ライブラリ読み込み
+const express = require('express');
+
+const app = express();
+const bodyParser = require('body-parser');
+
+// body-parserの設定
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 3001; // port番号を指定
+
+// GET http://localhost:3000/api/v1/
+app.get('/v1/', (req, res) => {
+  res.json({
+    message: 'Hello, GAE APP',
+  });
+});
+
+// サーバ起動
+console.log(`listen on port ${port}`);
+app.listen(port);
