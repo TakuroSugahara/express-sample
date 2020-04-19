46969b50b0f01d4f5d1ef5591c1c29bd2859b798
TakuroSugahara <k5690033@gmail.com>
2020-04-19 15:01:32 +0900

    feat: dbへの接続

--- app/app.js
+++ app/app.js
@@ -1,5 +1,16 @@
 // ライブラリ読み込み
+const env = () => {
+  if (process.env.NODE_ENV === 'production') {
+    return 'prod';
+  }
+  return 'dev';
+};
+
+const ENV_FILE = `${__dirname}/../.${env()}.env`;
+require('dotenv').config({ path: ENV_FILE });
+
 const express = require('express');
+const { Client } = require('pg');
 
 const app = express();
 const bodyParser = require('body-parser');
@@ -10,11 +21,50 @@ app.use(bodyParser.json());
 
 const port = process.env.PORT || 8080; // port番号を指定
 
+console.log({
+  database: process.env.DATABASE_DB,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT),
+});
+
+const client = new Client({
+  database: process.env.DATABASE_DB,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+});
+
+try {
+  client.connect();
+} catch (e) {
+  console.log('cannot connect db');
+  console.log(e);
+}
+
 // GET http://localhost:3000/api/v1/
 app.get('/v1/', (req, res) => {
   res.json({
-    message: 'Hello, GAE APP',
+    message: 'hello, gae app',
   });
+});
+
+app.get('/db/', async (req, res) => {
+  try {
+    const response = await client.query('SELECT * FROM "any"."user"');
+    console.log('sql query response', response);
+    res.json({
+      message: 'SELECT * FROM "any"."user"',
+      datas: response.rows,
+    });
+  } catch (err) {
+    console.error(err);
+    res.json({
+      message: 'Error',
+    });
+  }
 });
 
 // サーバ起動
